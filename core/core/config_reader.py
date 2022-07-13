1fc61ad4fa9360d3c57477a9563d8ef433af464e
Aleksandr Teslenko <alteslo31@gmail.com>
2022-07-13 15:03:37 +0300

    Зreconfiguration config_reader.py

---
+++ core/core/config_reader.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings, SecretStr
+
+
+class Settings(BaseSettings):
+    django_secret_key: SecretStr
+    django_debug_options: bool
+    django_allowed_hosts: str
+    DJANGO_ALLOWED_HOSTS: str
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+        env_nested_delimiter = '__'
+
+
+config = Settings()
